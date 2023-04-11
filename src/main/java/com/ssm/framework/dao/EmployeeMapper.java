8ea6d3b4ff6570b3209ab8cc51b302475f7ea978
RothschildsMa <73640448+RothschildsMa@users.noreply.github.com>
2023-04-11 23:10:29 +0900

    ①myBatis初期設定 ②entity作成 ③mapperインタフェース作成 ④テーブル作成 ⑤service作成

--- src/main/java/com/ssm/framework/dao/EmployeeMapper.java
+++ src/main/java/com/ssm/framework/dao/EmployeeMapper.java
@@ -1,10 +1,16 @@
 package com.ssm.framework.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.ssm.framework.entity.Employee;
 
 
 
 @Mapper
 public interface EmployeeMapper {
+	
+	 List<Employee> findAll();
 
 }
--- src/main/java/com/ssm/framework/entity/Employee.java
+++ src/main/java/com/ssm/framework/entity/Employee.java
@@ -1,7 +1,59 @@
 package com.ssm.framework.entity;
 
+import java.io.Serializable;
+import java.util.Date;
 
+import lombok.Data;
 
-public class Employee {
+@Data
+public class Employee implements Serializable{
+	
+	//自動採番ID
+	private Long id;
+	
+	//社員ID
+	private String employeeId;
+	
+	//パスワード
+	private String password;
+	
+	//入社年月日
+	private Date joinDate;
+	
+	//社員名称漢字
+	private String employeeName;
+	
+	//社員名称カナ
+	private String employeeNameKana;
+	
+	//性別ID
+	private String employeeGenderId;
+	
+	//年齢
+	private int employeeAge;
+	
+	//所属ID
+	private String employeeDepartmentId;
+	
+	//電話番号
+	private String employeePhoneNumber;
+	
+	//社員メールアドレス
+	private String employeeMail;
+
+	//削除フラグ
+	private int delFlg;
+
+	//作成日時
+	private Date createDate;
+
+	//作成者id
+	private int createUser;
+
+	//更新日時
+	private Date updateDate;
+
+	//更新者id
+	private int updateUser;
 
 }
