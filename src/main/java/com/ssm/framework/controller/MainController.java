d778a197bdd6c406aea0953fd3841b8464f04911
RothschildsMa <73640448+RothschildsMa@users.noreply.github.com>
2023-04-11 13:18:27 +0900

    springMVC sample codeをアップロードしました。

---
+++ src/main/java/com/ssm/framework/controller/MainController.java
@@ -0,0 +1,31 @@
+package com.ssm.framework.controller;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("view1")
+	public String start() {
+		return "input";
+	}
+	
+	@PostMapping("confirm")
+	public String confirmView(Model model,@RequestParam String name, @RequestParam Integer age,
+			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth) {
+		
+		
+		model.addAttribute("name",name);
+		model.addAttribute("age",age);
+		model.addAttribute("birth",birth);
+		
+		return "confirm";
+	}
+}
