65c562e70acfec21dd7eeb4551100b63ab4020f3
RothschildsMa <73640448+RothschildsMa@users.noreply.github.com>
2023-04-11 16:25:48 +0900

    aaa

--- src/main/java/com/ssm/framework/controller/MainController.java
+++ src/main/java/com/ssm/framework/controller/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-	//5
+	//6
 	@GetMapping("view1")
 	public String start() {
 		return "input";
