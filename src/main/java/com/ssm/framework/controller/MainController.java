ae7476cdbb2e32e227e3c835cb4c74c12fd98f0a
tarou917 <hamataro.0719@gmail.com>
2023-04-12 13:38:56 +0900

    test2追加

--- src/main/java/com/ssm/framework/controller/MainController.java
+++ src/main/java/com/ssm/framework/controller/MainController.java
@@ -14,77 +14,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.ssm.framework.entity.Employee;
 import com.ssm.framework.service.EmployeeService;
 
-
 @Controller
 public class MainController {
-	//DI(依存性注入)　spring 根幹※
+	//DI(依存性注入)　spring 根幹※　//test2
 	//7
 	@GetMapping("view2") //URLマッピング
 	public String start() { //リクエスト　ハンドラ　メソッド
 		return "team2/input";
 	}
-	
-	
+
 	@Autowired
-    private EmployeeService employeeService;
-    
-	//DB取得&出力サンプル1
-    @GetMapping(value = "/employee/list")
-    public String showList(Model model) {
-    	//ローワーキャメルケース　命名規則
-        List<Employee> empList = employeeService.findAll();
-        model.addAttribute("employeeList", empList);
-        return "output";
-    }
-	
+	private EmployeeService employeeService;
+
+	//DB取得&出力サンプル
+	@GetMapping(value = "/employee/list")
+	public String showList(Model model) {
+		List<Employee> empList = employeeService.findAll();
+		model.addAttribute("employeeList", empList);
+		return "output";
+	}
+
 	//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-	
+
 	@GetMapping("login")
 	public String loginView() {
 		return "team1/Login";
 	}
-	
-	
+
 	@GetMapping("akimi")
 	public String restart() {
 		return "team2/akimi";
 	}
-	
-	@GetMapping("view3")
-	public String employInformationDisplay() {
-		return "team2/employInformationDisplay";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
 	//HTTPメソッド
 	@PostMapping("confirm")
-	public String confirmView(Model model,@RequestParam String name, @RequestParam Integer age,
+	public String confirmView(Model model, @RequestParam String name, @RequestParam Integer age,
 			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth) {
-		
-		
-		model.addAttribute("name",name);
-		model.addAttribute("age",age);
-		model.addAttribute("birth",birth);
-		
+
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		model.addAttribute("birth", birth);
+
 		return "confirm";
 	}
 }
