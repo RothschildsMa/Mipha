6facd0d3de57abc09d2f3b3f0740965b334e0a6a
tarou917 <hamataro.0719@gmail.com>
2023-04-12 17:55:59 +0900

    INSERT文追加

--- src/main/java/com/ssm/framework/service/EmployeeService.java
+++ src/main/java/com/ssm/framework/service/EmployeeService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ssm.framework.dao.EmployeeMapper;
 import com.ssm.framework.entity.Employee;
+import com.ssm.framework.form.Form;
 
 @Service
 public class EmployeeService {
@@ -14,9 +15,15 @@ public class EmployeeService {
 	//社員情報マッパ
 	@Autowired
 	private EmployeeMapper employeeMapper;
+
 	//社員情報すべて検索
-	public List<Employee> findAll(){
+	public List<Employee> findAll() {
 		return employeeMapper.findAll();
+
 	}
-	
+
+	public void add(Form form) {
+		employeeMapper.add(form);
+	}
+
 }
