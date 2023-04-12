c2ab8f1a94ef72454869f3edebeb7671477c1e30
tarou917 <hamataro.0719@gmail.com>
2023-04-12 17:43:47 +0900

    insert文追加

--- src/main/java/com/ssm/framework/dao/EmployeeMapper.java
+++ src/main/java/com/ssm/framework/dao/EmployeeMapper.java
@@ -6,13 +6,12 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.ssm.framework.entity.Employee;
 
-
-
 @Mapper
 public interface EmployeeMapper {
-	
+
 	//社員情報全て検索
 	List<Employee> findAll();
-	
+
+	void add();
 
 }
