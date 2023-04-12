3cf873a3bffe2e7c0818141a1275ccefc8a60d62
RothschildsMa <73640448+RothschildsMa@users.noreply.github.com>
2023-04-12 11:38:34 +0900

    サンプル追加 ThymeleafでDBデータ出力

--- src/main/java/com/ssm/framework/controller/MainController.java
+++ src/main/java/com/ssm/framework/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.ssm.framework.controller;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ssm.framework.entity.Employee;
+import com.ssm.framework.service.EmployeeService;
+
 
 @Controller
 public class MainController {
@@ -17,6 +23,18 @@ public class MainController {
 	public String start() { //リクエスト　ハンドラ　メソッド
 		return "team2/input";
 	}
+	
+	
+	@Autowired
+    private EmployeeService employeeService;
+    
+	//DB取得&出力サンプル
+    @GetMapping(value = "/employee/list")
+    public String showList(Model model) {
+        List<Employee> empList = employeeService.findAll();
+        model.addAttribute("employeeList", empList);
+        return "output";
+    }
 	
 	//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 	
