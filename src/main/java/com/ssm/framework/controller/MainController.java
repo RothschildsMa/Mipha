5c4522228638241e9f1bb6c2ccc05965c5fac41a
RothschildsMa <73640448+RothschildsMa@users.noreply.github.com>
2023-04-14 09:29:59 +0900

    要らないコード消した

--- src/main/java/com/ssm/framework/controller/MainController.java
+++ src/main/java/com/ssm/framework/controller/MainController.java
@@ -1,28 +1,17 @@
 package com.ssm.framework.controller;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ssm.framework.entity.Employee;
 import com.ssm.framework.service.EmployeeService;
 
 @Controller
 public class MainController {
-
-	//DI(依存性注入)　spring 根幹※ test
-	//7
-	@GetMapping("view2") //URLマッピング
-	public String start() { //リクエスト　ハンドラ　メソッド
-		return "team2/input";
-	}
 
 	@Autowired
 	private EmployeeService employeeService;
@@ -42,8 +31,6 @@ public class MainController {
 		return "team2/akimi";
 	}
 
-	//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-
 	@GetMapping("login")
 	public String loginView() {
 		return "team1/Login";
@@ -54,19 +41,4 @@ public class MainController {
 		return "team2/akimi";
 	}
 
-	//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-
-	//HTTPメソッド
-	@PostMapping("confirm")
-	public String confirmView(Model model, @RequestParam String name, @RequestParam Integer age,
-			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth) {
-
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
-		model.addAttribute("birth", birth);
-
-		return "confirm";
-	}
 }
-//testHama
-//testZ
