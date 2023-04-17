78839db7949af49ea6b669508a8a9b8aeecebcbd
RothschildsMa <73640448+RothschildsMa@users.noreply.github.com>
2023-04-17 16:04:06 +0900

    Merge branch 'master' of https://github.com/RothschildsMa/Mipha.git

--- src/main/java/com/ssm/framework/controller/MainController.java
+++ src/main/java/com/ssm/framework/controller/MainController.java
@@ -20,8 +20,6 @@ public class MainController {
 	private EmployeeService employeeService;
 
 	//DB取得&出力サンプル
-	
-	
 	@GetMapping(value = "/employee/list")
 	public String showList(Model model) {
 		List<Employee> empList = employeeService.findAll();
