3a9fbc83e78bdd51323e1d30bc716afb97eb7310
RothschildsMa <73640448+RothschildsMa@users.noreply.github.com>
2023-04-17 17:01:35 +0900

    search

--- src/main/java/com/ssm/framework/controller/MainController.java
+++ src/main/java/com/ssm/framework/controller/MainController.java
@@ -19,7 +19,7 @@ public class MainController {
 	@Autowired
 	private EmployeeService employeeService;
 
-	//DB取得&出力サンプル
+	//debugリスト表示
 	@GetMapping(value = "/employee/list")
 	public String showList(Model model) {
 		List<Employee> empList = employeeService.findAll();
@@ -27,34 +27,39 @@ public class MainController {
 		return "output";
 	}
 
-
-	@GetMapping(value = "/employee/list1")
-	public String showList1(Model model) {
-		List<Employee> empList = employeeService.findAll();
-		model.addAttribute("employeeList", empList);
-		return "team2/employdemo";
+	@GetMapping("login")
+	public String loginView() {
+		return "team1/Login";
 	}
-
-	@GetMapping(value = "/employee/list2")
-	public String showList2(Model model) {
+	
+	//社員情報一覧画面
+	@GetMapping(value = "/emp/info")
+	public String employeeInformation(Model model) {
 		List<Employee> empList = employeeService.findAll();
 		model.addAttribute("employeeList", empList);
 		model.addAttribute("form", new Form());
 		return "team2/emp2";
 	}
 
+	//社員情報登録画面
 	@GetMapping(value = "/emp/add")
 	public String displayAdd(Model model) {
 		model.addAttribute("form", new Form());
 		return "team2/register2";
 	}
 
+	//社員情報登録処理
 	@RequestMapping(value = "/emp/insert", method = RequestMethod.POST)
 	public String addToTable(Form form) {
 		// 社員情報の登録
 		employeeService.add(form); //情報挿入
-		return "redirect:/employee/list"; //リダイレクト
+		return "redirect:/emp/info"; //リダイレクト
 	}
+	
+	
+	
+	
+	
 
 	@RequestMapping(value = "/emp/update", method = RequestMethod.POST)
 	public String updateToTabel(Form form) {
@@ -63,17 +68,7 @@ public class MainController {
 		return "redirect:/employee/list"; //リダイレクト
 	}
 
-	@GetMapping("login")
-	public String loginView() {
-		return "team1/Login";
-	}
-
-	
-	@GetMapping("akimi")
-	public String restart() {
-		return "team2/akimi";
-	}
-
+	//条件検索 完成(社員ID) 未完成(所属ID、入社年月日範囲チェック)
 	@RequestMapping(value = "/employee/search", method = RequestMethod.POST)
 	public String getEmployeesByCondition(Model model, Form form) {
 		List<Employee> employees = employeeService.findByCondition(form);
@@ -81,5 +76,6 @@ public class MainController {
 		return "team2/emp2";
 
 	}
+
 
 }
