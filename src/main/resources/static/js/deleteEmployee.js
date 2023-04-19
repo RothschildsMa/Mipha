1d25fcc1a6c6920904c2707ca9589d0f09cce2dd
tarou917 <hamataro.0719@gmail.com>
2023-04-19 09:47:02 +0900

    濱jsファイル修正

--- src/main/resources/static/js/deleteEmployee.js
+++ src/main/resources/static/js/deleteEmployee.js
@@ -1,12 +1,15 @@
 function deleteEmployees() {
   var employeeIds = [];
   $("input[name='employeeIds']:checked").each(function () {
-    employeeIds.push($(this).val());
+    employeeIds.push($(this).closest('tr').find('td:eq(1)').text());
   });
   if (employeeIds.length > 0) {
     if (confirm("選択された社員を削除しますか？")) {
-      $("#employeeIds").val(employeeIds.join(','));
-      $("#deleteForm").submit();
+      var form = $("#deleteForm");
+      for (var i = 0; i < employeeIds.length; i++) {
+        form.append("<input type='hidden' name='employeeIds' value='" + employeeIds[i] + "'>");
+      }
+      form.submit();
     }
   } else {
     alert("削除する社員を選択してください。");
