5c6057baf470af482940a682dbfb72a854b8a4e5
MichaelKarls <44009465+MichaelKarls@users.noreply.github.com>
2023-04-27 11:19:00 +0900

    チョウセキ

--- src/main/resources/static/js/urlManager.js
+++ src/main/resources/static/js/urlManager.js
@@ -22,3 +22,32 @@ function submitForm(formId,keyName,method = undefined) {
     if(method !== undefined) form.method = method;    // フォームのmethod属性にHTTPメソッドを設定
     form.submit(); // フォームを送信
 }
+
+function deleteData() {
+        var checkboxes = document.getElementsByName("selectedEmployees");
+        var checked = false;
+        for(var i = 0; i < checkboxes.length; i++){
+			if (checkboxes[i].checked) {
+				checked = true;
+          		break;
+        	}
+		}
+		
+		if (checked) {
+			if(confirm("選択した社員情報一覧を削除してもよろしいですか。")){
+				submitForm('selectForm','delete','post');
+				 //<!-- location.reload(); -->
+			}else{
+				event.preventDefault(); //submitキャンセル
+			}
+        } else {
+			alert("削除する社員を選択してください。");
+			event.preventDefault(); //submitキャンセル
+        }
+      }
+
+ //MD5 暗号化パスワードの使用
+ function md5(password) {
+  const md5 = crypto.createHash('md5');
+  return md5.update(password).digest('hex');
+}
